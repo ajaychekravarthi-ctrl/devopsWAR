aceb7a5b9215faf1d51bc14bc6f1651358b76210
ajaychekravarthi-ctrl <ajaychekravarthi@gmail.com>
2025-11-16 12:09:56 +0530

    Add HelloServlet to respond with a greeting

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>Hello from ajey WAR deployed in Tomcat!</h2>");
+    }
+}
